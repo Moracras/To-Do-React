1c0368527caf35b9fc0ce0545ae56204e874d3e1
Moracras <tavfikkarademir@gmail.com>
2023-12-13 20:14:25 +0300

    edt

---
+++ src/components/Header.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <>
+    
+    <div className='d-flex align-center justify-content-center mt-3'>
+    <img src="https://w7.pngwing.com/pngs/972/511/png-transparent-todo-sketch-note-list-tasks-thumbnail.png" alt="HeaderImage" />
+    </div>
+    <h1 style={{color:"white"}} className='text-center' >To-Do-App</h1>
+    
+    
+    </>
+    
+    
+  )
+}
+
+export default Header
