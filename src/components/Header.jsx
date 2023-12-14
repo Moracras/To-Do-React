9f3ee73c673a77ae0ed948b25969913dc2e85855
Moracras <tavfikkarademir@gmail.com>
2023-12-14 20:07:37 +0300

    progressin

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,13 +1,23 @@
 import React from 'react'
+import WebFont from "webfontloader"
+import { useEffect } from 'react'
+
 
 const Header = () => {
+  useEffect(() => {
+    WebFont.load({
+      google: {
+        families: ['Droid Sans', 'Chilanka']
+      }
+    });
+   }, []);
   return (
     <>
     
     <div className='d-flex align-center justify-content-center mt-3'>
     <img src="https://w7.pngwing.com/pngs/972/511/png-transparent-todo-sketch-note-list-tasks-thumbnail.png" alt="HeaderImage" />
     </div>
-    <h1 style={{color:"white"}} className='text-center' >To-Do-App</h1>
+    <h1 style={{color:"#E7D3DA",fontFamily: "Chilanka",fontWeight:"700"}} className='text-center' >To-Do-App</h1>
     
     
     </>
